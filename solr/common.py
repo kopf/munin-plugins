6aa2089fc5d360339a968cac70e81859d2d6c8a3
Aengus Walton <ventolin@gmail.com>
2012-03-21 14:36:33 +0100

    Add error handling for munin

--- solr/common.py
+++ solr/common.py
@@ -1,13 +1,19 @@
+import sys
+
 import requests
 
 from settings import AUTH, SERVER, PORT
 
 def make_request(path):
-    if AUTH:
-        r = requests.get('http://%s:%s%s' %(SERVER, PORT, path),
-                         auth=AUTH)
-    else:
-        r = requests.get('http://%s:%s%s' %(SERVER, PORT, path))
+    try:
+        if AUTH:
+            r = requests.get('http://%s:%s%s' %(SERVER, PORT, path),
+                             auth=AUTH)
+        else:
+            r = requests.get('http://%s:%s%s' %(SERVER, PORT, path))
+    except Exception, e:
+        print e
+        sys.exit(-1)
     if r.status_code != 200:
         return None
     return r.text
